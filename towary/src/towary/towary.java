a32feba97037893436695cf13cb39192f9eaecc5
Yuriy Shestakov <yshestakov@gmail.com>
2016-05-07 20:03:16 +0300

    Fixed random number generator code

--- towary/src/towary/towary.java
+++ towary/src/towary/towary.java
@@ -6,29 +6,25 @@ public class towary {
 
     public static void main(String[] args) {
     	
-    	for(int i = 0; i <10000; i++){
+    	String currentUsersHomeDir = System.getProperty("user.home");
+    	String towary_fn = currentUsersHomeDir + File.separator + "Towary.txt";
+    			
         
-        try(FileWriter writer = new FileWriter("C:\\Users\\Dasha\\Towary.txt", true))
+        try(FileWriter writer = new FileWriter(towary_fn, true))
         {
-           
-        	int i1 = i;
-        	String str = " ";
-        	int i2 = (int)Math.random()*100;
-            writer.write(i1 + str + i2);
-      
-            writer.append('\n'); 
-            
-             
-            writer.flush(); }
-      
-        catch(IOException ex){
-             
-            System.out.println(ex.getMessage());
+        	for(int i=0; i < 10000; i++)
+        	{
+	        	int i1 = i;
+	        	String str = " ";
+	        	int i2 = (int)(Math.random()*100);
+	            writer.write(i1 + str + i2);
+	            writer.append('\n'); 
+            }
+            writer.flush();
         }
-        
-       }
-            
+        catch(IOException ex){    
+            System.err.println(ex.getMessage());
+        }
     } 
-
 }
 
