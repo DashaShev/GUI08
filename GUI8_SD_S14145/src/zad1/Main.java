8d011a0998860ecdcf71a6447b91cf2739fea56f
DashaShev <sdasha321@gmail.com>
2016-05-12 21:43:01 +0200

    Update Main.java

--- GUI8_SD_S14145/src/zad1/Main.java
+++ GUI8_SD_S14145/src/zad1/Main.java
@@ -6,17 +6,33 @@
 
 package zad1;
 
-import java.io.*;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedTransferQueue;
 
 public class Main {
 
-  public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) {
 	  
 	TowarReader towar = new TowarReader();
   	
   	Thread thr = new Thread(towar);
   	thr.start(); 
+  	
+  	LinkedTransferQueue<Object> towarTransQueue = new LinkedTransferQueue<Object>();
   
+  	ExecutorService exService = Executors.newFixedThreadPool(2);
+  	Runnable producer = new TowarReader(towarTransQueue);
+  	Runnable consumer = new TowarProcessor(towarTransQueue);
+	exService.execute(producer);
+	exService.execute(consumer);
+	exService.shutdown();
+    
+	  
+	 
 	  
   }
 }
